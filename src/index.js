31fc32e95b731cea187e8f88f87d5df995961ba8
Salomon Laleye <salomonlaleye@gmail.com>
2022-11-20 14:54:15 +0100

    Load CSS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import _ from "lodash";
+import "./style.css";
 
 function MyComponent() {
   console.log("Hi from MyComponent");
   const element = document.createElement("div");
 
   // Lodash now importeds by this script
-  element.innerHTML = _.join(["Hello", "there", "webpack"], " ");
-
+  element.innerHTML = _.join(["Hello", "there", "webpack!"], " ");
+  element.classList.add("greeting");
   return element;
 }
 
